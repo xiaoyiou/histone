a0aaac0b9825bdfa2b1379a8eca70228771ee82e
Yiou Xiao <yixiao@syr.edu>
2017-03-29 16:00:11 -0400

    March 28

--- test.py
+++ test.py
@@ -1,13 +1,49 @@
 # This script is a test for hetero association learning using non-zero labels
 
 from eonetwork import mapper,eval
+import numpy as np
+import matplotlib.pyplot as plt
+import nengo
+# from nengo import spa
+#
+#
+# dim = 32
+# vocab = spa.Vocabulary(dimensions=dim)
+#
+# words = ['ORANGE', 'APRICOT', 'CHERRY', 'STRAWBERRY', 'APPLE']
+#
+# for word in words:
+#     vocab.parse(word)
+#
+#
+#
+#
+#
+
+res = {'pmloss':[], 'hloss':[], 'mlsetting':[], 'f1':[]}
+nh_res = {'pmloss':[], 'hloss':[], 'mlsetting':[], 'f1':[]}
 
 learner = mapper.Assoc(hopfield=True, keepz=False)
-A, B = learner.loaddata(all, go, 'F')
+nh_learner = mapper.Assoc(hopfield=False, keepz=False)
+
+A, B = learner.loaddata(all, go, 'F',useslim=False)
 
 # removing all-zero entries
-inds = [x[0] for x in(B==1).any(axis=1).tolist()]
-A = A[inds, :]
-B = B[inds, :]
+#inds = [x[0] for x in(B==1).any(axis=1).tolist()]
+sizes = []
 
-learner.train(A, B)
+for i in xrange(10):
+    inds = [x[0] for x in ((B == 1).sum(axis=1) > i).tolist()]
+    if sum(inds) == 0:
+        break
+    a = A[inds, :]
+    b = B[inds, :]
+    sizes.append(a.shape[0])
+    learner.train(a, b)
+    nh_learner.train(a, b)
+    zz = learner.recall(a)
+    nh_zz = nh_learner.recall(a)
+    for method in res:
+        res[method].append(eval.eval(zz, b,type=method))
+        nh_res[method].append(eval.eval(nh_zz, b,type=method))
+
